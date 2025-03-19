628565764ef4a7b8a731b467de5a832bd0b5042a
Mehedi Hasan <mehedi.hasan23@brainstation-23.com>
2025-03-19 16:09:09 +0600

    feat: implement CRUD operations in BaseService and update package exports

--- packages/services/src/core/base.service.ts
+++ packages/services/src/core/base.service.ts
@@ -1,18 +1,55 @@
-import { Entity } from '@diucsealumni/core';
+import { Entity, HttpMethod } from '@diucsealumni/core';
 import { PagedContent } from './paged-content';
 import { Query, QueryFilter } from './query';
 
 export class BaseService<T extends Entity> {
     protected url: string;
 
+    constructor(url: string) {
+        this.url = url;
+    }
+
     async getById(id: string) {
-        const response = await fetch(this.url + '/' + id);
+        const url = `${this.url}/${id}`;
+        const response = await fetch(url);
         return await response.json() as T;
     }
 
     async getPaged(page: number, filter: QueryFilter) {
         const queryString = new Query(page, filter).getQueryString();
-        const response = await fetch(this.url + queryString);
+        const url = `${this.url}?${queryString}`;
+        const response = await fetch(url);
         return await response.json() as PagedContent<T>;
+    }
+
+    async create(entity: T) {
+        const response = await fetch(this.url, {
+            method: HttpMethod.POST,
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(entity),
+        });
+        return await response.json() as T;
+    }
+
+    async update(id: string, entity: T) {
+        const url = `${this.url}/${id}`;
+        const response = await fetch(url, {
+            method: HttpMethod.PATCH,
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(entity),
+        });
+        return await response.json() as T;
+    }
+
+    async delete(id: string) {
+        const url = `${this.url}/${id}`;
+        const response = await fetch(url, {
+            method: HttpMethod.DELETE,
+        });
+        return response.ok;
     }
 }
